4196e881d853ecc808388247dc2a4344954397be
Javinator11 <sosa.puga.javier@gmail.com>
2021-10-06 16:34:39 -0600

    First Commit

---
+++ src/components/Routes/index.tsx
@@ -0,0 +1,19 @@
+import {Switch, Route} from 'react-router-dom'
+import ProductDetail from "../../containers/ProductDetail";
+import ProductList from "../../containers/ProductList";
+
+/**
+ * Define Routes
+ * @returns Routes
+ */
+
+ const Routes = () => {
+     return(
+        <Switch>
+             <Route path="/" component={ProductList} exact/>
+             <Route path="/pdp" component={ProductDetail}/>
+        </Switch>
+     );
+ };
+
+ export default Routes;
